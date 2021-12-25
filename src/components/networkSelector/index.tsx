44b7cd04be430818ad68d352f862d331f926dbc5
Ali Haghighatkhah <ali@lightcurve.io>
2021-12-26 00:46:06 +0100

    Create network switcher

---
+++ src/components/networkSelector/index.tsx
@@ -0,0 +1,32 @@
+import React, { ChangeEvent } from 'react';
+import { networkNames } from '../../constants';
+import './networkSelector.css';
+
+interface NetworkSelectorProps {
+  onNetworkChanged: (network: string) => void
+}
+
+const NetworkSelector = ({ onNetworkChanged }: NetworkSelectorProps) => {
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    onNetworkChanged(e.target.value);
+  };
+
+  return (
+    <form className="networkSelector">
+      <select onChange={onChange}>
+        {
+          Object.keys(networkNames).map((item) => (
+            <option
+              value={item}
+              key={item}
+            >
+              {networkNames[item].title}
+            </option>
+          ))
+        }
+      </select>
+    </form>
+  );
+}
+
+export default NetworkSelector;
