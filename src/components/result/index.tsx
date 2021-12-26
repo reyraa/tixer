8eed3f8245ff8bc168e062452be35291d7be73ba
Ali Haghighatkhah <ali@lightcurve.io>
2021-12-26 01:38:49 +0100

    Display results

--- src/components/result/index.tsx
+++ src/components/result/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import QRCode from "react-qr-code";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { isEmpty } from '../../utils/helpers';
+import { txToHex } from '../../utils/transaction';
 import './result.css';
 
 interface ResultProps {
@@ -6,9 +10,29 @@ interface ResultProps {
 }
 
 const Result = ({ value }: ResultProps) => {
+  const [json, setJson] = useState<any>({});
+  const [copied, setCopied] = useState<boolean>(false);
+  useEffect(() => {
+    const val = txToHex(value);
+    setJson(JSON.stringify(val));
+  }, [value]);
+
   return (
     <section className="result">
-      { JSON.stringify(value) }
+       {
+         isEmpty(value)
+          ? null
+          : (
+            <div>
+              <QRCode value={json} />
+              <CopyToClipboard text={json} onCopy={() => setCopied(true)}>
+                  <button>
+                    { copied ? 'Copied' : 'Copy' }
+                  </button>
+              </CopyToClipboard>
+            </div>
+          )
+      }
     </section>
   );
 }
